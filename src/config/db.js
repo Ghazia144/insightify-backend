1994d3bfed069189a3170f3f96bd9a29d1fe5c0f
Ghazia Sheikh <ghaziasheikh749@gmail.com>
2026-01-12 00:34:01 +0500

    fix case sensitivity

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log("URI Check:", process.env.MONGO_URI ? "Found" : "Not Found"); 
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected ✔");
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -36,3 +36,4 @@ app.use("/api/settings", require("./routes/settingsRoutes"));
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
+module.exports = app;
